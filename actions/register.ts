eaaee9f8f07921ff472169397fa022441846e785
YanalSabbagh <y.sabbagh@smart-zebra.de>
2024-03-10 00:18:11 +0100

    create a verificationToken model, add a verification token on register,

--- actions/register.ts
+++ actions/register.ts
@@ -5,6 +5,7 @@ import { RegisterSchema} from "@/schemas";
 import bcrypt from "bcryptjs";
 import {db} from "@/lib/db";
 import {getUserByEmail} from "@/data/user";
+import {generateVerificationToken} from "@/lib/tokens";
 
 export const register = async (values : z.infer<typeof RegisterSchema> ) => {
     // validate the fields
@@ -28,8 +29,7 @@ export const register = async (values : z.infer<typeof RegisterSchema> ) => {
             password:hashedPassword
         }
     });
-
     // TODO: Send verification token email...
-
-    return { success: "User Created successfully!"};
+    const verificationToken = await generateVerificationToken(email);
+    return {success : "Confirmation email sent!" };
 };
