7af351f7516301cb32b15e27f2c89f3854939c77
YanalSabbagh <y.sabbagh@smart-zebra.de>
2024-03-09 23:34:46 +0100

    deal with the same email in diffrent provider error and add the error to our custom login page...

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -3,7 +3,9 @@
 import React, {useState} from 'react';
 import {useTransition} from "react";
 import { useForm } from "react-hook-form";
+import {useSearchParams} from "next/navigation";
 import {zodResolver} from "@hookform/resolvers/zod";
+
 import {Form, FormControl, FormField, FormItem , FormLabel,FormMessage} from "@/components/ui/form";
 import {CardWrapper} from "@/components/auth/card-wrapper";
 import {LoginSchema} from "@/schemas";
@@ -15,6 +17,11 @@ import {FormSuccess} from "@/components/form-success";
 import {login} from "@/actions/login";
 
 const LoginForm = () => {
+    const searchParams = useSearchParams();
+    const urlError = searchParams.get("error") === "OAuthAccountNotLinked"
+        ? "Email is already in use with diffrent provider"
+        : "";
+
     const[error,setError] = useState<string | undefined>("");
     const[success,setSuccess] = useState<string | undefined>("");
     const [isPending, startTransition] = useTransition();
@@ -79,7 +86,7 @@ const LoginForm = () => {
                             )}
                         />
                     </div>
-                    <FormError message={error} />
+                    <FormError message={error || urlError} />
                     <FormSuccess message={success} />
                     <Button
                         className={"w-full"}
