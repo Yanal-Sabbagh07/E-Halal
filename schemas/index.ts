5751cdb049957f850a8f755bce02435477d9b4b3
YanalSabbagh <y.sabbagh@smart-zebra.de>
2024-03-17 21:35:41 +0100

    create a newPasswordSetter Form, and update the password in the User table

--- schemas/index.ts
+++ schemas/index.ts
@@ -14,3 +14,7 @@ export const RegisterSchema = z.object({
 export const ResetSchema = z.object({
     email: z.string().email({message: "Email is required"}),
 });
+
+export const NewPasswordSchema = z.object({
+    password: z.string().min(8,{message: "Minimum of 8 characters required!"}),
+});
